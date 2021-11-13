94384bc8444c279e4434b6a5a191cd78fb479d39
Angus Berg <angus.kw.berg@gmail.com>
2021-11-13 15:59:28 +1100

    Initial implementation of return class completed

---
+++ WordSearch_Classes.py
@@ -0,0 +1,28 @@
+"""
+########################################################################################################################
+# CUSTOM CLASSES USED BY THE WORD SEARCH PROGRAM
+# Author:           Angus Berg
+# Date Created:     12/11/2021
+########################################################################################################################
+"""
+from typing import Tuple
+
+"""
+#######################################################################
+# CLASS TO HOLD THE RESULTS OF A WORD FOUND IN THE WORD SEARCH
+#######################################################################
+"""
+class WordSearchResult:
+    #Initialisation function for the results class
+    def __init__(self, word: str, startPosition: Tuple[int, int], endPosition: Tuple[int, int]):
+        self.word: str = word
+        self.startX: int = startPosition[0]
+        self.startY: int = startPosition[1]
+        self.endX: int = endPosition[0]
+        self.endY: int = endPosition[1]
+
+    #Function to convert the contents of the results class into the output line for the results file
+    def createOutputLine(self) -> str:
+        startPart = "(" + str(self.startX) + ", " + str(self.startX) + ")"
+        endPart = "(" + str(self.endX) + ", " + str(self.endY) + ")"
+        return self.word + " " + startPart + " " + endPart
